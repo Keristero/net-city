9979987f7904867198dea4f21880f3e7c10a3186
ninjaman255 <aaronclevette@gmail.com>
2022-05-25 10:45:34 +0000

    Changes to make it temporarily usable

---
+++ Net-City.bat
@@ -0,0 +1,1 @@
+net_battle_server.exe -p 6900 
---
+++ assets/tiles/home_warp.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="home_warp" tilewidth="60" tileheight="32" tilecount="5" columns="5" objectalignment="top">
+ <image source="home_warp.png" width="300" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="52" height="30">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="1" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
