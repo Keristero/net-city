90539e6c15d234596ed03ee813ff9031c097ce2f
Keristero Kerivinix <Keristero@gmail.com>
2022-05-26 17:31:11 +1200

    fix building draw offset (works for small navi's anyway)

--- assets/tiles/test_floor.tsx
+++ assets/tiles/test_floor.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="test_floor" tilewidth="64" tileheight="32" tilecount="18" columns="2">
  <image source="test_floor.png" width="128" height="288"/>
+ <tile id="2" type="Ice"/>
 </tileset>
