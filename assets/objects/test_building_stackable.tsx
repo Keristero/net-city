047f7d3f5fd234b9bfcccb167ac4810b9bd80ceb
Keristero Kerivinix <Keristero@gmail.com>
2022-05-26 17:00:56 +1200

    fix building stacking so that it works for 80px tall navis

---
+++ assets/objects/test_building_stackable.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="test_building_stackable" tilewidth="192" tileheight="192" tilecount="1" columns="1">
+ <tileoffset x="0" y="48"/>
+ <image source="test_building_stackable.png" width="192" height="192"/>
+</tileset>
