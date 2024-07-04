d78433539d6021868a247abc978f9f93e97a68d8
keristero <keristero@gmail.com>
2024-07-04 22:41:28 +1200

    add collisions to special

--- assets/objects/special.tsx
+++ assets/objects/special.tsx
@@ -1,8 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="special.png" tilewidth="22" tileheight="83" tilecount="32" columns="8" objectalignment="bottom">
+<tileset version="1.10" tiledversion="1.10.2" name="special.png" tilewidth="22" tileheight="83" tilecount="32" columns="8" objectalignment="bottom">
  <tileoffset x="0" y="41"/>
  <image source="special.png" width="176" height="332"/>
  <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.55622" y="37.5378" width="14.7517" height="7.50757">
+    <ellipse/>
+   </object>
+  </objectgroup>
   <animation>
    <frame tileid="0" duration="50"/>
    <frame tileid="1" duration="50"/>
@@ -15,6 +20,11 @@
   </animation>
  </tile>
  <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.62415" y="37.7462" width="14.7517" height="7.50757">
+    <ellipse/>
+   </object>
+  </objectgroup>
   <animation>
    <frame tileid="8" duration="50"/>
    <frame tileid="9" duration="50"/>
